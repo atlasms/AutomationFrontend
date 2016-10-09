e122d54bc589be6f992f2f05dcc485e0c7948aa8
Farid Rn <faridv@gmail.com>
2016-10-09 23:51:23 +0330

    Fixed bugs and improved User model

--- app/user/user.model.js
+++ app/user/user.model.js
@@ -1,11 +1,5 @@
-define([
-    "jquery"
-    , "underscore"
-    , "backbone"
-    , "router"
-    , "config"
-    , "localstorage"
-], function ($, _, Backbone, Router, Config, Storage) {
+define(["jquery", "underscore", "backbone", "config"
+], function ($, _, Backbone, Config) {
     var UserModel = Backbone.Model.extend({
         defaults: {
             storageKey: ''
@@ -14,14 +8,14 @@ define([
         , initialize: function (options) {
             this.storageKey = Config.title.toLowerCase() + '_' + window.location.host.replace(/\./g, '').split(":")[0];
             //            this.storage = new Storage(this.storageKey);
-            console.log('Login Model Init');
+//            console.log('Login Model Init');
         }
         , authorize: function () {
             var items = STORAGE.getItem(this.storageKey);
             if (items) {
                 var content = JSON.parse(items);
-                console.log(content);
                 if (content.username && content.token) {
+                    console.log('User found: ' + JSON.stringify(content));
                     return true;
                 }
             }
@@ -29,8 +23,8 @@ define([
         }
         , redirect: function () {
             // Redirecting User to login page
-            !Backbone.History.started && Backbone.history.start({ pushState: true });
-            new Backbone.Router().navigate('login', { trigger: true });
+            !Backbone.History.started && Backbone.history.start({pushState: true});
+            new Backbone.Router().navigate('login', {trigger: true});
         }
         , login: function (form) {
             var key = this.storageKey;
@@ -50,8 +44,8 @@ define([
                     STORAGE.setItem(key, JSON.stringify(userData));
                     STORAGE.setItem(key + '_token', d.token);
                     // Redirect to home
-                    !Backbone.History.started && Backbone.history.start({ pushState: true });
-                    new Backbone.Router().navigate('', { trigger: true });
+                    !Backbone.History.started && Backbone.history.start({pushState: true});
+                    new Backbone.Router().navigate('', {trigger: true});
                 }
             });
         }
