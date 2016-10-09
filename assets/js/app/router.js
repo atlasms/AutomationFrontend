55b3d18a7f645a5cd77bc181d2eb2be5dee836e3
Farid Rn <faridv@gmail.com>
2016-10-09 23:56:22 +0330

    Made routes dynamic

--- assets/js/app/router.js
+++ assets/js/app/router.js
@@ -1,30 +1,26 @@
 // Filename: router.js
-define([
-    "jquery"
-    , "underscore"
-    , "backbone"
-    , "login.view"
-    , 'template'
-    , 'config'
-//    , "app.master"
-], function ($, _, Backbone, Login, Template, Config) {
+define(["jquery", "underscore", "backbone", "login.view", 'template', 'config', "app.view"
+], function ($, _, Backbone, Login, Template, Config, AppView) {
     var AppRouter = Backbone.Router.extend({
         routes: {
             // Define some URL routes
-            '': 'index',
             'login': 'Login',
-            // Default
-            '*actions': 'index'
+            // Other routes
+            '*actions': 'app'
         }
         , Login: function () {
             var loginView = new Login();
             loginView.render();
         }
+        , app: function (actions) {
+            var app = new AppView();
+            app.load(actions);
+        }
     });
 
     var initialize = function () {
         var appRouter = new AppRouter;
-        appRouter.on("route:index", function () {
+        appRouter.on("route:app", function () {
             $("body").addClass("page-container-bg-solid page-sidebar-closed-hide-logo page-header-fixed page-footer-fixed");
             var template = Template.template.load('', 'app');
             template.done(function (data) {
@@ -35,7 +31,7 @@ define([
             });
         });
 
-        Backbone.history.start({ pushState: true });
+        Backbone.history.start({pushState: true});
     };
 
     return {
