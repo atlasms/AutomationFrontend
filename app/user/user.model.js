7965f9d1555ad7b5becb983bbb3196bc4299eff1
Farid Rn <faridv@gmail.com>
2016-10-14 17:18:48 +0330

    Handling login errors

--- app/user/user.model.js
+++ app/user/user.model.js
@@ -1,5 +1,5 @@
-define(["jquery", "underscore", "backbone", "config"
-], function ($, _, Backbone, Config) {
+define(["jquery", "underscore", "backbone", "config", 'toastr'
+], function ($, _, Backbone, Config, toastr) {
     var UserModel = Backbone.Model.extend({
         defaults: {
             storageKey: ''
@@ -19,33 +19,61 @@ define(["jquery", "underscore", "backbone", "config"
                     return true;
                 }
             }
-            return false;
+            this.redirect();
         }
         , redirect: function () {
             // Redirecting User to login page
             !Backbone.History.started && Backbone.history.start({pushState: true});
             new Backbone.Router().navigate('login', {trigger: true});
         }
+        , logout: function() {
+            this.redirect();
+        }
         , login: function (form) {
             var key = this.storageKey;
             $.ajax({
                 url: CONFIG.api.url + CONFIG.api.login
-                , data: JSON.stringify(form)
+                , data: form
                 , type: 'post'
                 , dataType: "json"
                 , success: function (d) {
                     if (d.status && d.status !== 200)
                         return false;
-//                    var data = d.body;
                     var userData = {
                         username: form.username
-                        , token: d.token
-                    }
+                        , token: d.token_type + ' ' + d.access_token
+                        , expire: new Date().getTime() + (parseInt(d.expires_in) * 1000)
+                    };
                     STORAGE.setItem(key, JSON.stringify(userData));
-                    STORAGE.setItem(key + '_token', d.token);
+                    STORAGE.setItem(key + '_token', d.token_type + ' ' + d.access_token);
                     // Redirect to home
                     !Backbone.History.started && Backbone.history.start({pushState: true});
                     new Backbone.Router().navigate('', {trigger: true});
+                }
+                , error: function (jqXHR, exception) {
+                    var msg = '';
+                    var type = 'error';
+                    if (jqXHR.status === 0) {
+                        msg = 'Not connect.\n Verify Network.';
+                    } else if (jqXHR.status === 400) {
+                        msg = 'درخواست نا معتبر. [400]';
+                    } else if (jqXHR.status === 403) {
+                        msg = 'عدم مجوز اجرای دستور. [403]';
+                        type = 'warning';
+                    } else if (jqXHR.status === 500) {
+                        msg = 'خطا در سرور. [500]';
+                    } else if (jqXHR.status === 503) {
+                        msg = 'خطا در اجرای دستور. [503]';
+                    } else if (exception === 'timeout') {
+                        msg = 'Time out error.';
+                    } else if (exception === 'abort') {
+                        msg = 'Request aborted.';
+                        type = 'warning';
+                    } else {
+                        msg = 'Uncaught Error.\n' + jqXHR.responseText;
+                    }
+                    toastr.error(msg, 'خطا', {positionClass: 'toast-bottom-left', progressBar: true, closeButton: true});
+//                    TODO: Toast
                 }
             });
         }
