dcb968bfdc6a5e9aa12c345417e8f84fe8db9bc0
Farid Rn <faridv@gmail.com>
2016-10-15 17:59:17 +0330

    Working on toolbar (added input support)

--- assets/js/helpers/toolbar.js
+++ assets/js/helpers/toolbar.js
@@ -15,7 +15,16 @@ define(['jquery', 'underscore', 'backbone', 'config'
             var text = (typeof args.text !== "undefined") ? args.text : 'Submit';
             var type = (typeof args.type !== "undefined") ? args.type : 'submit';
             var affix = (typeof args.affix !== "undefined") ? 'append' : 'prepend';
-            var output = '<button type="' + type + '" class="' + cssClass + '">' + text + '</button>';
+            var output = '<div class="form-group"><button type="' + type + '" class="' + cssClass + '">' + text + '</button></div>';
+            this.toolbar = (affix === "prepend") ? output + this.toolbar : this.toolbar + output;
+        }
+        , input: function (args) {
+            var cssClass = (typeof args.cssClass !== "undefined") ? args.cssClass : 'form-control';
+            var placeholder = (typeof args.placeholder !== "undefined") ? args.placeholder : '';
+            var type = (typeof args.type !== "undefined") ? args.type : 'text';
+            var name = (typeof args.name !== "undefined") ? args.name : '';
+            var affix = (typeof args.affix !== "undefined") ? 'append' : 'prepend';
+            var output = '<div class="form-group"><input type="' + type + '" class="' + cssClass + '" name="' + name + '" placeholder="' + placeholder + '" /></div>';
             this.toolbar = (affix === "prepend") ? output + this.toolbar : this.toolbar + output;
         }
     });
