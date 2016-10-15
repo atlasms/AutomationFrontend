d3a9243bb56263e38e673edef470b1a57d17bb16
Farid Rn <faridv@gmail.com>
2016-10-15 17:59:55 +0330

    Working on schedules view

--- app/broadcast/schedule/schedule.view.js
+++ app/broadcast/schedule/schedule.view.js
@@ -1,11 +1,12 @@
-define(['jquery', 'underscore', 'backbone', 'template', 'config', 'moment-with-locales', 'broadcast.schedule.model', 'mask', 'toolbar'
-], function ($, _, Backbone, Template, Config, moment, ScheduleModel, Mask, Toolbar) {
+define(['jquery', 'underscore', 'backbone', 'template', 'config', 'moment-with-locales', 'broadcast.schedule.model', 'mask', 'toolbar', 'pdatepicker'
+], function ($, _, Backbone, Template, Config, moment, ScheduleModel, Mask, Toolbar, pDatepicker) {
     var ScheduleView = Backbone.View.extend({
         el: $(Config.positions.main)
         , model: 'ScheduleModel'
         , toolbar: [
-            {'button': {cssClass: 'btn btn-success', text: 'جدید', type: 'button'}}
-            , {'button': {cssClass: 'btn btn-info', text: 'salam salam', type: 'button'}}
+              {'button': {cssClass: 'btn btn-success', text: 'جدید', type: 'button'}}
+            , {'input': {cssClass: 'form-control datepicker', placeholder: '', type: 'text', name: 'enddate'}}
+            , {'input': {cssClass: 'form-control datepicker', placeholder: '', type: 'text', name: 'startdate'}}
         ]
         , events: {
             'submit': 'submit'
@@ -31,18 +32,24 @@ define(['jquery', 'underscore', 'backbone', 'template', 'config', 'moment-with-l
                         var handlebarsTemplate = Template.handlebars.compile(data);
                         var output = handlebarsTemplate(items);
                         $container.html(output).promise().done(function () {
-                            self.renderToolbar();
                             self.afterRender();
                         });
                     });
                 }
             });
+            self.renderToolbar();
         }
         , afterRender: function () {
             var $timePickers = $(".time");
+            var $datePickers = $(".datepicker");
             $.each($timePickers, function () {
                 $(this).mask('H0:M0:S0', {
                     placeholder: '00:00:00', translation: {'H': {pattern: /[0-2]/}, 'M': {pattern: /[0-5]/}, 'S': {pattern: /[0-5]/}}
+                });
+            });
+            $.each($datePickers, function() {
+                $(this).pDatepicker({
+                    format: 'YYYY-MM-DD HH:mm:ss'
                 });
             });
         }
