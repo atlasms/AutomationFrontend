09d1bc66626909e8c1c92bb04a597802876487e0
Farid Rn <faridv@gmail.com>
2016-10-16 23:10:27 +0330

    Added Schedule helper class for further usage

---
+++ app/broadcast/schedule/schedule.helper.js
@@ -0,0 +1,7 @@
+define(['jquery', 'underscore', 'backbone', 'config', 'jdate'
+], function ($, _, Backbone, Config, jDate) {
+    var ScheduleHelper = {
+        
+    };
+    return ScheduleHelper;
+});
