48ab3bf7c1a1ec4ada5099f4cb108b0ff0dabace
Farid Rn <faridv@gmail.com>
2016-10-19 20:30:59 +0330

    Warning user about losing unsaved data

--- app/broadcast/schedule/schedule.view.js
+++ app/broadcast/schedule/schedule.view.js
@@ -116,6 +116,15 @@ define(['jquery', 'underscore', 'backbone', 'template', 'config', 'global', 'mom
         , prepareContent: function () {
             // var model = new ScheduleModel();
             // model.initialize();
+            $(document).on('change', 'tr input', function () {
+                var myEvent = window.attachEvent || window.addEventListener;
+                var chkevent = window.attachEvent ? 'onbeforeunload' : 'beforeunload'; /// make IE7, IE8 compitable
+                myEvent(chkevent, function (e) { // For >=IE7, Chrome, Firefox
+                    var confirmationMessage = 'Are you sure to leave the page?';  // a space
+                    (e || window.event).returnValue = confirmationMessage;
+                    return confirmationMessage;
+                });
+            });
         }
         , prepareSave: function () {
             var data = [];
