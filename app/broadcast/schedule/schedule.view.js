8251fcff59907fb9541a3a65d977d559c840125a
Farid Rn <faridv@gmail.com>
2016-10-20 21:19:23 +0330

    Working on duplicatation "sub-toolbar"

--- app/broadcast/schedule/schedule.view.js
+++ app/broadcast/schedule/schedule.view.js
@@ -1,13 +1,14 @@
-define(['jquery', 'underscore', 'backbone', 'template', 'config', 'global', 'moment-with-locales', 'broadcast.schedule.model', 'mask', 'toolbar', 'pdatepicker', 'scheduleHelper'
-], function ($, _, Backbone, Template, Config, Global, moment, ScheduleModel, Mask, Toolbar, pDatepicker, ScheduleHelper) {
+define(['jquery', 'underscore', 'backbone', 'template', 'config', 'global', 'moment-with-locales', 'broadcast.schedule.model', 'mask', 'toastr', 'toolbar', 'pdatepicker', 'scheduleHelper', 'bootstrap/transition'
+], function ($, _, Backbone, Template, Config, Global, moment, ScheduleModel, Mask, toastr, Toolbar, pDatepicker, ScheduleHelper) {
     var ScheduleView = Backbone.View.extend({
         el: $(Config.positions.wrapper)
         , model: 'ScheduleModel'
         , toolbar: [
-            {'button': {cssClass: 'btn green-jungle pull-right hide fade', text: 'ذخیره', type: 'submit', task: 'save'}}
+            {'button': {cssClass: 'btn purple-wisteria pull-right', text: 'کپی', type: 'button', task: 'show-subtoolbar'}}
+            , {'button': {cssClass: 'btn green-jungle pull-right hidden fade', text: 'ذخیره', type: 'submit', task: 'save'}}
             , {'button': {cssClass: 'btn c-btn-border-1x c-btn-grey-salsa', text: 'PDF', type: 'pdf', task: 'file'}}
             , {'button': {cssClass: 'btn btn-success', text: 'نمایش', type: 'button', task: 'load'}}
-//            , {'input': {cssClass: 'form-control datepicker hide', placeholder: '', type: 'text', name: 'enddate', value: persianDate().format('YYYY-MM-DD')}}
+//            , {'input': {cssClass: 'form-control datepicker hidden', placeholder: '', type: 'text', name: 'enddate', value: persianDate().format('YYYY-MM-DD')}}
             , {'input': {cssClass: 'form-control datepicker', placeholder: '', type: 'text', name: 'startdate', value: persianDate().format('YYYY-MM-DD')}}
         ]
         , flags: {}
@@ -16,6 +17,9 @@ define(['jquery', 'underscore', 'backbone', 'template', 'config', 'global', 'mom
             , 'keyup input.time': 'processTime'
             , 'click [data-task=load]': 'load'
             , 'click [data-task=file]': 'loadFile'
+            , 'click [data-task=show-subtoolbar]': 'showSubtoolbar'
+            , 'click [data-task=duplicate]': 'duplicate'
+            , 'change [name=force]': 'warnForceDuplicate'
         }
         , submit: function () {
             var helper = new ScheduleHelper.validate();
@@ -27,6 +31,41 @@ define(['jquery', 'underscore', 'backbone', 'template', 'config', 'global', 'mom
                 , contentType: 'application/json'
                 , processData: false
             });
+        }
+        , duplicate: function (e) {
+            e.preventDefault();
+            var params = {
+                startdate: Global.jalaliToGregorian($("#duplicate-schedule [name=startdate]").val()) + 'T00:00:00'
+                , enddate: Global.jalaliToGregorian($("#duplicate-schedule [name=startdate]").val()) + 'T23:59:59'
+                , destinationdate: Global.jalaliToGregorian($("#duplicate-schedule [name=destinationdate]").val()) + 'T00:00:00'
+                , force: +$("#duplicate-schedule [name=force]").val()
+            };
+            new ScheduleModel({path: '/copy'}).save(null, {
+                data: JSON.stringify(params)
+                , contentType: 'application/json'
+                , processData: false
+                , error: function (e, data) {
+                    toastr.error(data.responseJSON.Message, 'خطا', {positionClass: 'toast-bottom-left', progressBar: true, closeButton: true});
+                }
+                , success: function () {
+                    toastr.success('با موفقیت انجام شد', 'عملیات کپی', {positionClass: 'toast-bottom-left', progressBar: true, closeButton: true});
+                    $("#sub-toolbar").removeClass("in");
+                }
+            });
+        }
+        , warnForceDuplicate: function (e) {
+            var $checkbox = $(e.target);
+            if ($checkbox.is(":checked"))
+                $("#schedule-overwrite-alert").addClass('in');
+            else
+                $("#schedule-overwrite-alert").removeClass('in');
+        }
+        , showSubtoolbar: function () {
+            if ($("#sub-toolbar").is(":hidden"))
+                $("#sub-toolbar").addClass("in");
+            else
+                $("#sub-toolbar").removeClass("in");
+            $("html, body").animate({'scrollTop': 0});
         }
         , loadFile: function (e) {
             var params = {
@@ -72,7 +111,7 @@ define(['jquery', 'underscore', 'backbone', 'template', 'config', 'global', 'mom
         }
         , afterRender: function () {
             ScheduleHelper.mask("time");
-            $("#toolbar button[type=submit]").removeClass('hide').addClass('in');
+            $("#toolbar button[type=submit]").removeClass('hidden').addClass('in');
             if (typeof this.flags.helperLoaded === "undefined") {
                 ScheduleHelper.init();
                 this.flags.helperLoaded = true;
