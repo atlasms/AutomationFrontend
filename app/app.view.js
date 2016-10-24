610451e577fdf4ca92e50004e046d3d2fe11e670
Farid Rn <faridv@gmail.com>
2016-10-24 20:03:58 +0330

    Added 404 error and minor improvements in master layout

--- app/app.view.js
+++ app/app.view.js
@@ -1,19 +1,26 @@
 define(['jquery', 'underscore', 'backbone', 'template', 'config', 'cookie', 'layout', 'bootstrap/dropdown'
 ], function ($, _, Backbone, Template, Config, Cookies, Layout, Dropdown) {
     var AppView = Backbone.View.extend({
-        el: $(Config.positions.main)
+        el: $(Config.positions.wrapper)
         , render: function () {
             this.$el.html('<h1>App</h1>');
         }
         , load: function (actions) {
+            if (actions === 404) {
+                this.render404();
+                return;
+            }
             if (typeof actions !== "undefined" && actions) {
-                var request = (actions + '.view').replace(/\//g, '.');
+                var request = (actions.replace(/\$/, '') + '.view').replace(/\//g, '.');
                 requirejs([request], function (View) {
                     var view = new View();
                     var content = view.prepareContent();
                     view.render(content);
                 });
             }
+        }
+        , render404: function () {
+            this.$el.html('<h1 class="text-center">404!</h1>');
         }
     });
     return AppView;
