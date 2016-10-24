13d3dd3692c9e54760bd063fd32a940007f3bf25
Farid Rn <faridv@gmail.com>
2016-10-24 20:06:33 +0330

    Added router map

--- assets/js/app/router.js
+++ assets/js/app/router.js
@@ -13,22 +13,59 @@ define(["jquery", "underscore", "backbone", "login.view", 'template', 'config', 
             loginView.render();
         }
         , app: function (actions) {
-            var app = new AppView();
-            app.load(actions);
+
+        }
+        , map: {
+            "/": {private: true, access: null}
+            , "broadcast": {private: true, access: null}
+            , "broadcast/schedule": {private: true, access: null}
+            , "broadcast/scheduleprint": {private: false, access: null, skipLayout: false}
         }
     });
 
-    var initialize = function () {
-        var appRouter = new AppRouter;
-        appRouter.on("route:app", function () {
-            $("body").addClass("page-container-bg-solid page-sidebar-closed-hide-logo page-footer-fixed");
-            var template = Template.template.load('', 'app');
-            template.done(function (data) {
-                var html = $(data).wrap('<p/>').parent().html();
-                var handlebarsTemplate = Template.handlebars.compile(html);
-                var output = handlebarsTemplate();
-                $(Config.positions.wrapper).html(output);
-            });
+    var initMasterLayout = function (actions) {
+        $("body").addClass("page-container-bg-solid page-sidebar-closed-hide-logo page-footer-fixed");
+        var template = Template.template.load('', 'app');
+        template.done(function (data) {
+            var html = $(data).wrap('<p/>').parent().html();
+            var handlebarsTemplate = Template.handlebars.compile(html);
+            var output = handlebarsTemplate();
+            $(Config.positions.wrapper).html(output);
+            var app = new AppView();
+            if (typeof app.load === "function")
+                app.load(actions);
+            else
+                app.load(404);
+        });
+    };
+
+    var initCleanLayout = function (actions) {
+        var app = new AppView();
+        if (typeof app.load === "function")
+            app.load(actions);
+        else
+            app.load(404);
+    };
+
+    var initialize = function (user) {
+        var appRouter = new AppRouter();
+        var map = appRouter.map;
+        appRouter.on("route:app", function (actions) {
+            actions = (actions == null) ? "/" : actions;
+            if (typeof map[actions] === "undefined") { /// show 404
+                var app = new AppView();
+                app.load(404);
+                return;
+            }
+            if (user.authorize(actions, map)) { // Page needs access, redirecting to login page
+                if (typeof map[actions].skipLayout !== "undefined" && map[actions].skipLayout === false) { // Page doesn't need master layout
+                    if (/print$/.test(actions))
+                        $("head link").length && $("head link").remove();
+                    initCleanLayout(actions);
+                } else { // Normal Routing
+                    initMasterLayout(actions);
+                }
+            }
         });
 
         Backbone.history.start({pushState: true});
