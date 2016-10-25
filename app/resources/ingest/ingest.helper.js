547f458b3ead37df109b66396e32fa01b868821d
Farid Rn <faridv@gmail.com>
2016-10-25 19:11:23 +0330

    Added time mask to Ingest helper

--- app/resources/ingest/ingest.helper.js
+++ app/resources/ingest/ingest.helper.js
@@ -9,6 +9,18 @@ define(['jquery', 'underscore', 'backbone', 'config', 'global', 'moment-with-loc
 
             }
         }
+        , mask: function (type) {
+            if (typeof type === "undefined")
+                return false;
+            switch (type) {
+                case 'time':
+//                    $("input.time").unmask();
+                    $("input.time").mask('H0:M0:S0', {
+                        placeholder: '00:00:00', translation: {'H': {pattern: /[0-2]/}, 'M': {pattern: /[0-5]/}, 'S': {pattern: /[0-5]/}}
+                    });
+                    break;
+            }
+        }
     };
     return IngestHelper;
 });
