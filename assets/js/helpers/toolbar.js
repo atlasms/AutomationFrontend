80ec674e306378b78b30c405f3014bc14e1fbeb3
Farid Rn <faridv@gmail.com>
2016-11-03 22:02:17 +0330

    Added select box for toolbar

--- assets/js/helpers/toolbar.js
+++ assets/js/helpers/toolbar.js
@@ -46,6 +46,22 @@ define(['jquery', 'underscore', 'backbone', 'config'
                 output += '<input type="' + type + '" class="' + cssClass + '" name="' + name + '" placeholder="' + placeholder + '" value="' + value + '" /></div></div>';
             this.toolbar = (affix === "prepend") ? output + this.toolbar : this.toolbar + output;
         }
+        , select: function(args) {
+            var cssClass = (typeof args.cssClass !== "undefined") ? args.cssClass : 'form-control';
+            var name = (typeof args.name !== "undefined") ? args.name : '';
+            var label = (typeof args.text !== "undefined") ? args.text : '';
+            var affix = (typeof args.affix !== "undefined") ? 'append' : 'prepend';
+            var addon = (typeof args.addon !== "undefined") ? args.addon : false;
+            var icon = (typeof args.icon !== "undefined") ? '<i class="' + args.icon + '"></i>' : '';
+            var options = '';
+            if (typeof args.options !== "undefined" && args.options.length)
+                for (var i = 0; i < args.options.length; i++)
+                    options += '<option value="' + args.options[i].value + '">' + args.options[i].text + '</option>';
+            var output = '<div class="form-group"><div class="input-group">';
+            output += addon ? '<span class="input-group-addon' + (icon !== "" ? ' has-icon' : '') + '">' + icon + '</span>' : '';
+                output += '<select data-type="' + name + '" class="' + cssClass + '" name="' + name + '">' + options + '</select></div></div>';
+            this.toolbar = (affix === "prepend") ? output + this.toolbar : this.toolbar + output;
+        }
     });
     return ToolbarHelper;
 });
