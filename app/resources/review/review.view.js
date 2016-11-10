7baae92385e425eb48ebb6a005ea9c45b1a3081a
Farid Rn <faridv@gmail.com>
2016-11-10 16:34:24 +0330

    Handling Review page event listeners

--- app/resources/review/review.view.js
+++ app/resources/review/review.view.js
@@ -1,8 +1,10 @@
-define(['jquery', 'underscore', 'backbone', 'template', 'config', 'global', 'moment-with-locales', 'resources.review.model', 'mask', 'toastr', 'toolbar', 'statusbar', 'pdatepicker', 'reviewHelper', 'jwplayer'
-], function ($, _, Backbone, Template, Config, Global, moment, ReviewModel, Mask, toastr, Toolbar, Statusbar, pDatepicker, ReviewHelper, jwplayer) {
+define(['jquery', 'underscore', 'backbone', 'template', 'config', 'global', 'jquery-ui', 'moment-with-locales', 'resources.review.model', 'mask', 'toastr', 'toolbar', 'statusbar', 'pdatepicker', 'reviewHelper', 'player.helper'
+], function ($, _, Backbone, Template, Config, Global, ui, moment, ReviewModel, Mask, toastr, Toolbar, Statusbar, pDatepicker, ReviewHelper, Player) {
     var ReviewView = Backbone.View.extend({
         el: $(Config.positions.wrapper)
+        , playerInstance: null
         , model: 'MetadataModel'
+        , $playerControls: $(".controls")
         , toolbar: [
             {'button': {cssClass: 'btn red pull-right', text: 'رد', type: 'button', task: 'reject'}}
             , {'button': {cssClass: 'btn green-jungle pull-right', text: 'قبول', type: 'button', task: 'accept'}}
@@ -19,13 +21,66 @@ define(['jquery', 'underscore', 'backbone', 'template', 'config', 'global', 'mom
         , flags: {}
         , events: {
             'click [type=submit]': 'submit'
-            , 'change [data-type=filter-table]': function (e) {
-                alert($(e.target).val());
+            , 'change [data-type=filter-table]': 'filterTable'
+            , 'click #review-table tbody tr': 'collapseRow'
+            , 'click a[data-type="controls"]': 'playerControls'
+        }
+        , playerControls: function (e) {
+//            alert()
+            e.preventDefault();
+            var $this = this;
+            var $target = $(e.currentTarget);
+            var task = $target.attr('data-task');
+            switch (task) {
+                case 'play-pause':
+                    $this.player.play(this.playerInstance, $(e.currentTarget));
+                    break;
             }
-            , 'click #review-table tbody tr': function (e) {
-                var target = ($(e.target).is("tr")) ? $(e.target) : $(e.target).parents("tr:first");
-//                $('<tr><td colspan="100%"><h1>a</h1></td></tr>').insertAfter(target);
-            }
+        }
+        , collapseRow: function (e) {
+            var $this = this;
+            var $el = $(e.target);
+            var $row = ($(e.target).is("tr")) ? $(e.target) : $(e.target).parents("tr:first");
+            var media = {
+                thumbnail: $row.attr('data-thumbnail')
+                , video: $row.attr('data-media')
+            };
+            var file = $row.attr('data-filename');
+            var file = $row.attr('data-filename');
+            if ($row.hasClass('active') || $row.hasClass('preview-pane') || $row.parents(".preview-pane").length || typeof media.video === "undefined")
+                return;
+            $el.parents("tbody").find("tr").removeClass('active');
+            $row.addClass('active');
+            if ($(document).find(".preview-pane").length)
+                $(document).find(".preview-pane").fadeOut(200, function () {
+                    var $target = $(this);
+                    $this.player.remove();
+                    window.setTimeout(function () {
+                        $target.remove();
+                    }, 50);
+                });
+            // Loading review partial template
+            window.setTimeout(function () {
+                var template = Template.template.load('resources/review', 'review.partial');
+                template.done(function (data) {
+                    var handlebarsTemplate = Template.handlebars.compile(data);
+                    var output = handlebarsTemplate({});
+                    $row.after(output).promise().done(function () {
+                        var player = new Player('#player-container', {
+                            image: media.thumbnail
+                            , file: media.video
+                        }, $this.handlePlayerCallbacks, $this.$playerControls);
+                        player.render();
+                        $this.player = player;
+                        $this.playerInstance = player.instance;
+                    });
+                });
+
+            }, 300);
+
+        }
+        , filterTable: function (e) {
+            alert($(e.target).val());
         }
         , submit: function () {
             var $this = this;
@@ -43,12 +98,6 @@ define(['jquery', 'underscore', 'backbone', 'template', 'config', 'global', 'mom
                 }
             });
         }
-        , selectRow: function (e) {
-            var $el = $(e.target);
-            var $row = $el.parents("tr:first");
-            $el.parents("tbody").find("tr").removeClass('active');
-            $row.addClass('active');
-        }
         , reLoad: function () {
             this.load();
         }
@@ -59,7 +108,6 @@ define(['jquery', 'underscore', 'backbone', 'template', 'config', 'global', 'mom
             this.render(params);
         }
         , render: function (params) {
-            console.log(jwplayer.api)
             var template = Template.template.load('resources/review', 'review');
             var $container = $(Config.positions.main);
             var model = new ReviewModel(params);
@@ -85,7 +133,37 @@ define(['jquery', 'underscore', 'backbone', 'template', 'config', 'global', 'mom
             });
         }
         , afterRender: function () {
+            var $this = this;
             ReviewHelper.mask("time");
+        }
+        , handlePlayerCallbacks: function (instance, type, value) {
+            var $this = this;
+            switch (type) { 
+                case 'setDuration':
+                    $("#seekbar .end").length && $("#seekbar .end").text(Global.createTime(value));
+                    break;
+                case 'setPosition':
+                    $("#seekbar .start").length && $("#seekbar .start").text(Global.createTime(value));
+                    break;
+                case 'initElements':
+                    $("#seekbar .seeker .inner:first").slider({
+                        value: 0
+                        , slide: function (e, ui) {
+                            instance.seek(ui.value);
+                        }
+                    });
+                    break;
+                case 'setSeekbar':
+                    $("#seekbar .seeker").find(".inner:first").slider('option', 'value', value);
+                    break;
+                case 'controls':
+                    var $playPause = this.$playerControls.find('[data-type="controls"][data-task="play-pause"]');
+                    if (value === "play")
+                        $playPause.attr('data-state', 'play');
+                    else
+                        $playPause.attr('data-state', 'pause');
+                    break;
+            }
         }
         , renderToolbar: function () {
             var self = this;
@@ -99,7 +177,7 @@ define(['jquery', 'underscore', 'backbone', 'template', 'config', 'global', 'mom
             });
             toolbar.render();
             self.flags.toolbarRendered = true;
-            
+
             var $datePickers = $(".datepicker");
             var datepickerConf = {
                 onSelect: function () {
