fb8516287e243138ff0064b8e7c89bf4705af360
Farid Rn <faridv@gmail.com>
2016-11-14 12:20:42 +0330

    Removed Dependencies list and added definiton loading

--- config.js
+++ config.js
@@ -1,119 +1,73 @@
-define({
-    "title": "اتوماسیون تولید و پخش",
-    "channel": "ایران‌کالا",
-    "siteUrl": "http://localhost",
-    "env": "dev",
-    "settings": {
-        "datepicker": {
-            "format": 'YYYY-MM-DD'
-            , "autoClose": true
-            , "observer": true
-            , "navigator": {
-                "enabled": true
-                , "text": {
-                    "btnNextText": ">"
-                    , "btnPrevText": "<"
+define(['jquery', 'underscore', 'backbone', 'global', 'definitions'], function ($, _, Backbone, Global, DefinitionsModel) {
+    /*
+     * Check localStorage for any pre-processed and cached configurations
+     */
+    /*
+     * Use following configurations
+     */
+    window.Config = {
+        "title": "اتوماسیون تولید و پخش",
+        "channel": "ایران‌کالا",
+        "siteUrl": "http://localhost",
+        "env": "dev",
+        "settings": {
+            "datepicker": {
+                "format": 'YYYY-MM-DD'
+                , "autoClose": true
+                , "observer": true
+                , "navigator": {
+                    "enabled": true
+                    , "text": {
+                        "btnNextText": ">"
+                        , "btnPrevText": "<"
+                    }
                 }
             }
+        },
+        "routes": {
+            "/": {"private": true, "access": null, "action": "DashboardView", "file": "dashboard/dashboard.view"}
+            , "broadcast": {"private": true, "access": null, "action": "BroadcastView", "file": "app/broadcast/broadcast.view"}
+            , "broadcast/schedule": {"private": true, "access": null, "action": "ScheduleView", "file": "app/broadcast/schedule/schedule.view"}
+            , "broadcast/scheduleprint": {"private": false, "access": null, "skipLayout": true, "action": "ScheduleView", "file": "app/broadcast/schedule/schedule.view"}
+            , "resources/ingest": {"private": true, "access": null, "action": "IngestView", "file": "app/resources/ingest/ingest.view"}
+            , "resources/metadata": {"private": true, "access": null, "action": "MetadataView", "file": "app/resources/metadata/metadata.view"}
+            , "resources/categories": {"private": true, "access": null, "action": "CategoriesView", "file": "app/resources/categories/categories.view"}
+            , "resources/review": {"private": true, "access": null, "action": "ReviewView", "file": "app/resources/review/review.view"}
+        },
+        "positions": {
+            "wrapper": "body"
+            , "main": "#main"
+            , "toolbar": "#toolbar"
+            , "status": "#status-items"
+        },
+        "api": {
+            "url": "http://93.190.24.246:81/api/"
+            , "login": "http://93.190.24.246:81/token"
+            , "schedule": "conductor"
+            , "tree": "metacategories"
+            , "storagefiles": "storagefiles"
+            , "metadata": "metadata"
+//            , "review": "metadata?categoryid=11"
+            , "definitions": "dfn"
         }
-    },
-    "routes": {
-        "/": {"private": true, "access": null, "action": "DashboardView", "file": "dashboard/dashboard.view"}
-        , "broadcast": {"private": true, "access": null, "action": "BroadcastView", "file": "app/broadcast/broadcast.view"}
-        , "broadcast/schedule": {"private": true, "access": null, "action": "ScheduleView", "file": "app/broadcast/schedule/schedule.view"}
-        , "broadcast/scheduleprint": {"private": false, "access": null, "skipLayout": true, "action": "ScheduleView", "file": "app/broadcast/schedule/schedule.view"}
-        , "resources/ingest": {"private": true, "access": null, "action": "IngestView", "file": "app/resources/ingest/ingest.view"}
-        , "resources/metadata": {"private": true, "access": null, "action": "MetadataView", "file": "app/resources/metadata/metadata.view"}
-        , "resources/categories": {"private": true, "access": null, "action": "CategoriesView", "file": "app/resources/categories/categories.view"}
-        , "resources/review": {"private": true, "access": null, "action": "ReviewView", "file": "app/resources/review/review.view"}
-    },
-    "positions": {
-        "wrapper": "body"
-        , "main": "#main"
-        , "toolbar": "#toolbar"
-        , "status": "#status-items"
-    },
-    "api": {
-        "url": "http://93.190.24.246:81/api/"
-        , "login": "http://93.190.24.246:81/token"
-        , "schedule": "conductor"
-        , "tree": "metacategories"
-        , "storagefiles": "storagefiles"
-        , "metadata": "metadata"
-        , "review": "metadata?categoryid=11"
-    },
-    "dependencies": [
-        {
-            "name": "jQuery",
-            "version": "3.1.1",
-            "folder": "vendor",
-            "type": "min",
-            "bundled": false,
-            "map": true
-        },
-        {
-            "name": "modernizr",
-            "version": "2.8.3",
-            "folder": "vendor",
-            "type": "min",
-            "bundled": true,
-            "map": false
-        },
-        {
-            "name": "respondJS",
-            "version": "1.4.2",
-            "folder": "vendor",
-            "type": "min",
-            "bundled": true,
-            "map": false
-        },
-        {
-            "name": "BackboneJS",
-            "version": "1.3.3",
-            "folder": "vendor",
-            "type": "min",
-            "bundled": false,
-            "map": true
-        },
-        {
-            "name": "requireJS",
-            "version": "2.3.2",
-            "folder": "vendor",
-            "type": "min",
-            "bundled": false,
-            "map": false
-        },
-        {
-            "name": "UnderscoreJS",
-            "version": "1.8.3",
-            "folder": "vendor",
-            "type": "min",
-            "bundled": false,
-            "map": true
-        },
-        {
-            "name": "Handlebars",
-            "version": "4.0.5",
-            "folder": "vendor",
-            "type": "min",
-            "bundled": false,
-            "map": false
-        },
-        {
-            "name": "MomentJS",
-            "version": "2.15.1",
-            "folder": "vendor",
-            "type": "min",
-            "bundled": false,
-            "map": false
-        },
-        {
-            "name": "MomentJS with Locales",
-            "version": "2.15.1",
-            "folder": "vendor",
-            "type": "min",
-            "bundled": false,
-            "map": false
+    };
+
+    /*
+     * Adding server-side definition params to main Config object
+     */
+    var model = new DefinitionsModel(Config);
+    model.fetch({
+        async: false
+        , success: function (items) {
+            items = items.toJSON();
+            for (var prop in Config)
+                delete items[prop];
+//            var items = $.map(items, function (value, index) {
+//                return [value];
+//            });
+            Config.definitions = items.Children;
         }
-    ]
+    });
+
+    return Config;
 });
