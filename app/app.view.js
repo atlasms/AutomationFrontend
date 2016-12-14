0f73f44aa4301acd8ec88ab59450165a35b6601f
Farid Rn <faridv@gmail.com>
2016-12-14 15:19:59 +0330

    Fixing bugs and improving main app

--- app/app.view.js
+++ app/app.view.js
@@ -1,11 +1,11 @@
-define(['jquery', 'underscore', 'backbone', 'template', 'config', 'cookie', 'layout'
-], function ($, _, Backbone, Template, Config, Cookies, Layout) {
+define(['jquery', 'underscore', 'backbone', 'template', 'config', 'cookie', 'layout', 'pace'
+], function ($, _, Backbone, Template, Config, Cookies, Layout, pace) {
     var AppView = Backbone.View.extend({
         el: $(Config.positions.wrapper)
         , render: function () {
             this.$el.html('<h1>App</h1>');
         }
-        , load: function (actions) {
+        , load: function (actions, actionArray) {
             if (actions === 404) {
                 this.render404();
                 return;
@@ -13,14 +13,20 @@ define(['jquery', 'underscore', 'backbone', 'template', 'config', 'cookie', 'lay
             if (typeof actions !== "undefined" && actions) {
                 var request = (actions === '' || actions === '/') ? 'dashboard.view' : (actions.replace(/\$/, '') + '.view').replace(/\//g, '.');
                 requirejs([request], function (View) {
-                    var view = new View();
+//                    pace.start();
+                    var view = new View(actions, actionArray);
                     var content = (typeof view.prepareContent !== "undefined") ? view.prepareContent() : null;
-                    view.render(content);
+                    view.render(content, actionArray);
+                    var $sidebarMenu = $(Config.positions.sidebar).find("ul:first");
+                    
+                    // Setting active menu
+                    $sidebarMenu.find("li").removeClass("active open");
+                    $sidebarMenu.find('a[href="/' + actions + '"]').parents("li").addClass("active open");
                 });
             }
         }
         , render404: function () {
-            $("body").attr('class', 'page-404-full-page')
+            $("body").attr('class', 'page-404-full-page');
             this.$el.html('<div class="page-404"><div class="number font-red"> 404 </div><div class="details"><h3>پیدا نشد!</h3><p>متاسفانه صفحه مورد نظر شما وجود ندارد.<br><a href="/"> بازگشت به داشبورد </a></p></div></div></div>');
         }
     });
