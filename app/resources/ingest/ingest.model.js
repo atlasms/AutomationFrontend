a17a455c662948fb743987433237990850d19645
Farid Rn <faridv@gmail.com>
2016-12-29 16:51:25 +0330

    Fixed bug in ingest app model

--- app/resources/ingest/ingest.model.js
+++ app/resources/ingest/ingest.model.js
@@ -5,8 +5,9 @@ define(["jquery", "underscore", "backbone", "config"
         , initialize: function (options) {
             this.query = (options && options.query) ? '?' + options.query : '';
             this.path = (options && options.path) ? options.path : '';
-            this.path = (options && options.id) ? '/' + options.id : '';
+            this.path = (options && options.id) ? '/' + options.id : this.path;
             this.overrideUrl = (options && options.overrideUrl) ? options.overrideUrl : '';
+            options = {};
         }
         , url: function () {
             if (this.overrideUrl !== "")
