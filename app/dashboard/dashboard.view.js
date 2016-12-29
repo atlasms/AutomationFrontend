ea459a9aec80cc9b127625c5c25fcfba19f68a60
Farid Rn <faridv@gmail.com>
2016-12-29 16:34:43 +0330

    Changed toolbar & statusbar initiation timing (dashboard)

--- app/dashboard/dashboard.view.js
+++ app/dashboard/dashboard.view.js
@@ -1,5 +1,5 @@
-define(['jquery', 'underscore', 'backbone', 'template', 'config', 'user'
-], function ($, _, Backbone, Template, Config, User) {
+define(['jquery', 'underscore', 'backbone', 'template', 'config', 'user', 'toolbar', 'statusbar'
+], function ($, _, Backbone, Template, Config, User, Toolbar, Statusbar) {
 
     var DashboardView = Backbone.View.extend({
         data: {}
@@ -9,6 +9,8 @@ define(['jquery', 'underscore', 'backbone', 'template', 'config', 'user'
             var self = this;
             var template = Template.template.load('dashboard', 'dashboard');
             template.done(function (data) {
+                new Toolbar().render();
+                new Statusbar().render();
                 var html = $(data).wrap('<p/>').parent().html();
                 var handlebarsTemplate = Template.handlebars.compile(html);
                 var output = handlebarsTemplate(this.data);
