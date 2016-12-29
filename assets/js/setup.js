efe9d507c3a95d836a3e894ea9bbe27339e54ba2
Farid Rn <faridv@gmail.com>
2016-12-29 16:38:27 +0330

    Added x-editables and improvements

--- assets/js/setup.js
+++ assets/js/setup.js
@@ -14,14 +14,15 @@ require.config({
         , "bootstrap/affix": {deps: ["jquery"], exports: "$.fn.affix"}
         , "bootstrap/alert": {deps: ["jquery"], exports: "$.fn.alert"}
         , "bootstrap/button": {deps: ["jquery"], exports: "$.fn.button"}
-        , "bootstrap/collapse":   { deps: ["jquery"], exports: "$.fn.collapse" }
+        , "bootstrap/collapse": {deps: ["jquery"], exports: "$.fn.collapse"}
         , "bootstrap/dropdown": {deps: ["jquery"], exports: "$.fn.dropdown"}
         , "bootstrap/modal": {deps: ["jquery"], exports: "$.fn.modal"}
-        , "bootstrap/popover":    { deps: ["jquery"], exports: "$.fn.popover" }
-        , "bootstrap/scrollspy":  { deps: ["jquery"], exports: "$.fn.scrollspy" }
+        , "bootstrap/scrollspy": {deps: ["jquery"], exports: "$.fn.scrollspy"}
         , "bootstrap/tab": {deps: ["jquery"], exports: "$.fn.tab"}
         , "bootstrap/tooltip": {deps: ["jquery"], exports: "$.fn.tooltip"}
+        , "bootstrap/popover": {deps: ["jquery", "bootstrap/tooltip"], exports: "$.fn.popover"}
         , "bootstrap/transition": {deps: ["jquery"], exports: "$.fn.transition"}
+        , "x-editable": {deps: ["jquery", "bootstrap/tooltip", "bootstrap/popover"], exports: "jQuery"}
         , "hotkeys": {deps: ["jquery"], exports: "jQuery"}
         , "jwplayer": {exports: "jwplayer"}
         , "ladda": {deps: ["spin"]}
@@ -51,6 +52,7 @@ require.config({
         , "spin": ["vendor/spin.min"]
         , "ladda": ["vendor/ladda.min"]
         , "pace": ["vendor/pace.min"]
+        , "x-editable": ["vendor/bootstrap-editable.min"]
 
         , "typeahead": ["vendor/typeahead.jquery.min"]
         , "bloodhound": ["vendor/bloodhound.min"]
@@ -88,6 +90,7 @@ require.config({
         , "resources.metadata.view": ["../../app/resources/metadata/metadata.view"]
         , "resources.categories.view": ["../../app/resources/categories/categories.view"]
         , "resources.review.view": ["../../app/resources/review/review.view"]
+        , "resources.returnees.view": ["../../app/resources/returnees/returnees.view"]
         , "resources.mediaitem.view": ["../../app/resources/mediaitem/mediaitem.view"]
         , "toolbar": ["helpers/toolbar"]
         , "statusbar": ["helpers/statusbar"]
@@ -107,13 +110,14 @@ require.config({
 //        , "metadataHelper": ["../../app/resources/ingest/categories.helper"]
         , "tree.helper": ["helpers/tree"]
         , "player.helper": ["helpers/player"]
+        , "editable.helper": ["helpers/editable"]
     }
 });
 require([
     'jquery', 'config', 'defines', 'app', 'router', 'user'
 ], function ($, Config, Defines, App, Router, User) {
     Defines.initialize();
-//    Defines.initialize();
+    var user = new User();
     var backboneSync = Backbone.sync;
     Backbone.sync = function (method, model, options) {
         /*
@@ -125,19 +129,20 @@ require([
         options = _.extend(options, {
             url: (((_.isFunction(model.url) ? model.url() : model.url)).indexOf('//') === -1 ? Config.api.url : '') + (_.isFunction(model.url) ? model.url() : model.url)
         });
+        if (user.token !== null) {
+            options = _.extend(options, {
+                headers: {"Authorization": user.token}
+            });
+        }
         /*
          *  Call the stored original Backbone.sync
          * method with the new url property
          */
         backboneSync(method, model, options);
     };
-    Router.initialize(new User());
-//    if (user.authorize())
+    Router.initialize(user);
     // The "app" dependency is passed in as "App"
     // Again, the other dependencies passed in are not "AMD" therefore don't pass a parameter to this function
     App.initialize();
-//    else {
-//        user.redirect();
-//    }
     return {};
 });
