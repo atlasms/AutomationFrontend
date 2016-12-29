2a1a0382f174440880f66c80ad21f94fe2dbd5b4
Farid Rn <faridv@gmail.com>
2016-12-29 16:41:25 +0330

    Fixed bugs in user logout and login

--- app/user/user.model.js
+++ app/user/user.model.js
@@ -5,20 +5,19 @@ define(["jquery", "underscore", "backbone", "config", 'toastr'
             storageKey: ''
         }
         , storage: {}
+        , token: ''
         , initialize: function (options) {
             this.storageKey = Config.title.toLowerCase() + '_' + window.location.host.replace(/\./g, '').split(":")[0];
-            //            this.storage = new Storage(this.storageKey);
-//            console.log('Login Model Init');
+            this.token = JSON.parse(STORAGE.getItem(this.storageKey)) && JSON.parse(STORAGE.getItem(this.storageKey)).token;
         }
         , authorize: function (actions, map) {
             if (typeof map[actions] !== "undefined" && map[actions].private === false)
                 return true;
             else {
-                var items = STORAGE.getItem(this.storageKey);
-                if (items) {
-                    var content = JSON.parse(items);
+                if (STORAGE.getItem(this.storageKey)) {
+                    var content = JSON.parse(STORAGE.getItem(this.storageKey));
                     if (content.username && content.token) {
-                        console.log('User found: ' + JSON.stringify(content));
+//                        console.log('User found: ' + JSON.stringify(content));
                         return true;
                     }
                 }
@@ -28,8 +27,10 @@ define(["jquery", "underscore", "backbone", "config", 'toastr'
         }
         , redirect: function () {
             // Redirecting User to login page
-            !Backbone.History.started && Backbone.history.start({pushState: true});
-            new Backbone.Router().navigate('login', {trigger: true});
+//            !Backbone.History.started && Backbone.history.start({pushState: true});
+//            new Backbone.Router().navigate('login', {trigger: true});
+            location.href = '/login';
+            return false;
         }
         , logout: function () {
             this.redirect();
@@ -50,10 +51,12 @@ define(["jquery", "underscore", "backbone", "config", 'toastr'
                         , expire: new Date().getTime() + (parseInt(d.expires_in) * 1000)
                     };
                     STORAGE.setItem(key, JSON.stringify(userData));
-                    STORAGE.setItem(key + '_token', d.token_type + ' ' + d.access_token);
+//                    STORAGE.setItem(key + '_token', d.token_type + ' ' + d.access_token);
                     // Redirect to home
-                    !Backbone.History.started && Backbone.history.start({pushState: true});
-                    new Backbone.Router().navigate('', {trigger: true});
+//                    !Backbone.History.started && Backbone.history.start({pushState: true});
+//                    new Backbone.Router().navigate('', {trigger: true});
+                    location.href = '/';
+                    return false;
                 }
                 , error: function (jqXHR, exception) {
                     var msg = '';
