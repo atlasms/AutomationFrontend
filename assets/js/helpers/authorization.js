0667c0172a88ec73afbd457ef367d2556c7e9fe8
Farid Rn <faridv@gmail.com>
2017-02-17 14:12:26 +0330

    Added authorization helper class

---
+++ assets/js/helpers/authorization.js
@@ -0,0 +1,12 @@
+define(['jquery', 'underscore', 'backbone', 'config', 'user.helper'], function ($, _, Backbone, Config, UserHelper) {
+    var Authorize = function(permissions, action, alias, params) {
+        var permit = false;
+        var key = (typeof action === "undefined") ? 'access' : action;
+        $.each(permissions, function() {
+            if (this.Key === key && this.Value === alias)
+                permit = true;
+        });
+        return permit;
+    };
+    return Authorize;
+});
