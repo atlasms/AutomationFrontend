02906f0a539b23a0f122097207cfce1978607d90
Farid Rn <faridv@gmail.com>
2017-02-17 14:12:37 +0330

    Loading menu based on user access

--- assets/js/app/router.js
+++ assets/js/app/router.js
@@ -1,5 +1,5 @@
-define(["jquery", "underscore", "backbone", "login.view", 'template', 'config', "layout", "user.helper", "notifications"
-], function ($, _, Backbone, Login, Template, Config, Layout, UserHelper, Notifications) {
+define(["jquery", "underscore", "backbone", "login.view", 'template', 'config', 'global', "layout", "user.helper", "notifications"
+], function ($, _, Backbone, Login, Template, Config, Global, Layout, UserHelper, Notifications) {
     var Router = Backbone.Router.extend({
         routes: {
             'login': 'Login'
@@ -44,9 +44,8 @@ define(["jquery", "underscore", "backbone", "login.view", 'template', 'config', 
                 var appAction = actions.replace(/\/+/g, '/').split('/').slice(0, 2).join('/').replace(/\/+$/, '');
                 appAction = (appAction === '') ? '/' : appAction;
                 if (typeof map[appAction] === "undefined") { /// show 404
-                    var app = new AppView();
-                    app.load(404);
-                    return;
+                    this.throwNotFound();
+                    return false;
                 }
                 if (UserHelper.authorize(appAction, map)) { // Page needs access, redirecting to login page
                     if (typeof map[appAction].skipLayout !== "undefined" && map[appAction].skipLayout === true) { // Page doesn't need master layout
@@ -56,6 +55,8 @@ define(["jquery", "underscore", "backbone", "login.view", 'template', 'config', 
                     } else { // Normal Routing
                         self.loadLayout(appAction);
                     }
+                } else {
+                    this.throwForbidden();
                 }
             });
             $(document).on("click", 'a[href^="/"]:not([target="_blank"])', function (evt) {
@@ -69,7 +70,9 @@ define(["jquery", "underscore", "backbone", "login.view", 'template', 'config', 
             });
             Backbone.history.start({pushState: true});
         }
-
+        , getMenu: function () {
+            return $.ajax({url: '/assets/js/app/menu.json'});
+        }
         , loadLayout: function (actions, clean) {
             var self = this;
             if (typeof clean !== "undefined" && clean === true) {
@@ -84,17 +87,40 @@ define(["jquery", "underscore", "backbone", "login.view", 'template', 'config', 
             $("body").addClass("has-master-layout page-container-bg-solid page-sidebar-closed-hide-logo page-footer-fixed page-header-fixed-mobile");
             var template = Template.template.load('', 'app');
             var user = UserHelper.getUser();
-            template.done(function (data) {
-                var handlebarsTemplate = Template.handlebars.compile($(data).wrap('<p/>').parent().html());
-                var output = handlebarsTemplate(user);
-                $(Config.positions.wrapper).html(output);
-                self.loadPage(actions);
-                // Initialize Layout helpers
-                Layout.init();
-                
-                // TODO: to replace current method with socket.io
-                new Notifications(user);
+            this.getMenu().done(function (rawMenu) {
+                Global.Cache.saveMenu(rawMenu);
+                var menu = self.prepareMenu(rawMenu);
+                ///
+                template.done(function (data) {
+                    var handlebarsTemplate = Template.handlebars.compile($(data).wrap('<p/>').parent().html());
+                    var output = handlebarsTemplate(user);
+                    $(Config.positions.wrapper).html(output);
+                    self.loadMenu(menu);
+                    self.loadPage(actions);
+                    // Initialize Layout helpers
+                    Layout.init();
+
+                    // TODO: to replace current method with socket.io
+                    new Notifications(user);
+                });
             });
+        }
+        , prepareMenu: function (data) {
+            data = $.grep(data, function (item, i) {
+                if (!UserHelper.Authorize('menu', item.alias))
+                    return false;
+                return true;
+            });
+            $.each(data, function () {
+                if (this.items.length) {
+                    this.items = $.grep(this.items, function (item) {
+                        if (!UserHelper.Authorize('menu', item.alias))
+                            return false;
+                        return true;
+                    });
+                }
+            });
+            return data;
         }
         , loadPage: function (actions) {
             var self = this;
@@ -110,6 +136,10 @@ define(["jquery", "underscore", "backbone", "login.view", 'template', 'config', 
             requirejs([request], function (View) {
 
                 // Instantiating new view
+                if (typeof View === "undefined") {
+                    self.throwError(undefined, Config.positions.main);
+                    return false;
+                }
                 var view = new View({actions: actions});
                 self.view = view;
 
@@ -122,6 +152,14 @@ define(["jquery", "underscore", "backbone", "login.view", 'template', 'config', 
                 $sidebarMenu.find('a[href="/' + actions + '"]').parents("li").addClass("active open");
             });
         }
+        , loadMenu: function (menu) {
+            var template = Template.template.load('shared', 'menu');
+            template.done(function (tmpl) {
+                var handlebarsTemplate = Template.handlebars.compile($(tmpl).wrap('<p/>').parent().html());
+                var output = handlebarsTemplate(menu);
+                $(Config.positions.sidebar).html(output);
+            });
+        }
         , loadContents: function (view, content, actionArray) {
             // Render view
             view.render(content, actionArray);
@@ -131,6 +169,18 @@ define(["jquery", "underscore", "backbone", "login.view", 'template', 'config', 
             var message = typeof message !== "undefined" ? message : 'متاسفانه صفحه مورد نظر شما وجود ندارد.';
             $("body").attr('class', 'page-404-full-page');
             $(Config.positions.wrapper).html('<div class="page-404"><div class="number font-red"> 404 </div><div class="details"><h3>پیدا نشد!</h3><p>' + message + '<br><a href="/"> بازگشت به داشبورد </a></p></div></div></div>');
+        }
+        , throwForbidden: function (message) {
+            var message = typeof message !== "undefined" ? message : 'شما دسترسی مشاهده این صفحه را ندارید';
+            $("body").attr('class', 'page-500-full-page');
+            $(Config.positions.wrapper).html('<div class="page-500"><div class="number font-red"> 403 </div><div class="details"><h3>عدم دسترسی!</h3><p>' + message + '<br><a href="/"> بازگشت به داشبورد </a></p></div></div></div>');
+        }
+        , throwError: function (message, position) {
+            var message = typeof message !== "undefined" ? message : 'مشکلی پیش آمده است';
+            var position = typeof position === "undefined" ? Config.positions.wrapper : position;
+            if (position === Config.positions.wrapper)
+                $("body").attr('class', 'page-500-full-page');
+            $(position).html('<div class="page-500"><div class="number font-red"> 500 </div><div class="details"><h3>خطا در درخواست!</h3><p>' + message + '<br></p></div></div></div>');
         }
     });
 
