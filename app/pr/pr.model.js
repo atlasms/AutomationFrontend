1cc9fed2cc24739a3bec641b182e7ba6b59d2730
Farid Rn <faridv@gmail.com>
2017-02-26 15:35:24 +0330

    Added PR app

---
+++ app/pr/pr.model.js
@@ -0,0 +1,23 @@
+define(["jquery", "underscore", "backbone", "config"
+], function ($, _, Backbone, Config) {
+    var PRModel = Backbone.Model.extend({
+        defaults: {}
+        , initialize: function (options) {
+            this.query = (options && options.query) ? '?' + options.query : '';
+            this.path = (options && options.path) ? options.path : '';
+            this.path = (options && options.id) ? '/' + options.id : this.path;
+            this.overrideUrl = (options && options.overrideUrl) ? options.overrideUrl : '';
+            options = {};
+        }
+        , url: function () {
+            if (this.overrideUrl !== "")
+                return this.overrideUrl + this.path + this.query;
+            else
+                return Config.api.sms + this.path + this.query;
+        }
+        , save: function (key, val, options) {
+            return Backbone.Model.prototype.save.call(this, key, val, options);
+        }
+    });
+    return PRModel;
+});
