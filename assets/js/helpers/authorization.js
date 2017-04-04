3464347e43cbe4916b42db28ebe258c9fb113d56
Farid Rn <faridv@gmail.com>
2017-04-04 11:28:09 +0430

    Improved Authorization helper

--- assets/js/helpers/authorization.js
+++ assets/js/helpers/authorization.js
@@ -1,12 +1,33 @@
-define(['jquery', 'underscore', 'backbone', 'config', 'user.helper'], function ($, _, Backbone, Config, UserHelper) {
-    var Authorize = function(permissions, action, alias, params) {
-        var permit = false;
-        var key = (typeof action === "undefined") ? 'access' : action;
-        $.each(permissions, function() {
-            if (this.Key == key && this.Value == alias)
-                permit = true;
-        });
-        return permit;
+define(['jquery', 'underscore', 'backbone', 'config', 'storage.helper'], function ($, _, Backbone, Config, Storage) {
+//    var Authorize = function (action, alias, permissions, params) {
+//        var permit = false;
+//        var key = (typeof action === "undefined") ? 'access' : action;
+//        var permissions = (typeof permissions === "undefined") ? Storage('Access') : permissions;
+//        $.each(permissions, function () {
+//            if (this.Key == key && this.Value == alias)
+//                permit = true;
+//        });
+//        return permit;
+//    };
+    var Authorize = {
+        access : function (action, permissions) {
+            var permit = false;
+            var permissions = (typeof permissions === "undefined") ? Storage('Access') : permissions;
+            $.each(permissions, function () {
+                if (this.Key === 'access' && this.Value == action)
+                    permit = true;
+            });
+            return permit;
+        }
+        , menu : function(action, permissions) {
+            var permit = false;
+            var permissions = (typeof permissions === "undefined") ? Storage('Access') : permissions;
+            $.each(permissions, function () {
+                if (this.Key === 'menu' && this.Value == action)
+                    permit = true;
+            });
+            return permit;
+        }
     };
     return Authorize;
 });
