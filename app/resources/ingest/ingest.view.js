800ba2c626bc57edb2f9478a9afda2662e1b3a38
Farid Rn <faridv@gmail.com>
2017-05-30 17:14:51 +0430

    REVERTED deleted files by mistake

---
+++ app/resources/ingest/ingest.view.js
@@ -0,0 +1,203 @@
+define(['jquery', 'underscore', 'backbone', 'template', 'config', 'global', 'resources.ingest.model', 'toastr', 'toolbar', 'statusbar', 'pdatepicker', 'ingestHelper', 'tree.helper', 'bootstrap/modal'
+], function ($, _, Backbone, Template, Config, Global, IngestModel, toastr, Toolbar, Statusbar, pDatepicker, IngestHelper, Tree) {
+    var IngestView = Backbone.View.extend({
+        $modal: "#metadata-form-modal"
+        , $metadataPlace: "#metadata-place"
+        , model: 'IngestModel'
+        , toolbar: [
+            {'button': {cssClass: 'btn purple-studio pull-right', text: '', type: 'button', task: 'refresh', icon: 'fa fa-refresh'}}
+            , {'button': {cssClass: 'btn blue-sharp disabled', text: 'ثبت اطلاعات ', type: 'button', task: 'add'}}
+        ]
+        , statusbar: []
+        , flags: {}
+        , events: {
+            'click [type=submit]': 'submit'
+            , 'click [data-task=refresh-view]': 'reLoad'
+            , 'click [data-task=add]': 'openAddForm'
+            , 'click [data-task=refresh]': 'loadStorageFiles'
+            , 'click #storagefiles tbody tr': 'selectRow'
+            , 'focus .has-error input': function (e) {
+                $(e.target).parents(".has-error:first").removeClass('has-error');
+            }
+        }
+        , submit: function (e) {
+            e.preventDefault();
+            var $this = this;
+            var helper = new IngestHelper.validate();
+            if (!helper.beforeSave())
+                return;
+            var data = this.prepareSave();
+            new IngestModel({overrideUrl: Config.api.metadata}).save(null, {
+                data: JSON.stringify(data)
+                , contentType: 'application/json'
+                , processData: false
+                , success: function () {
+                    toastr.success('با موفقیت انجام شد', 'ذخیره اطلاعات برنامه', {positionClass: 'toast-bottom-left', progressBar: true, closeButton: true});
+                    $($this.$modal).find("form").trigger('reset');
+                    $($this.$modal).modal('hide');
+                    $("#storagefiles tr.active").addClass('disabled').removeClass('active success');
+                }
+            });
+        }
+        , openAddForm: function (e) {
+            $(this.$modal).modal('toggle');
+        }
+        , selectRow: function (e) {
+            var $el = $(e.target);
+            var $row = $el.parents("tr:first");
+            if ($row.hasClass("disabled"))
+                return false;
+            $el.parents("tbody").find("tr").removeClass('active success');
+            $row.addClass('active success');
+            $row.find("[data-prop]").each(function () {
+                if ($('input[name="' + $(this).attr('data-prop') + '"]').length)
+                    $('input[name="' + $(this).attr('data-prop') + '"]').val($.trim($(this).text()));
+            });
+//            $('button[data-task=add]').removeClass('disabled').find("span").html($.trim($row.find('[data-type="index"]').text()));
+            $('button[data-task=add]').removeClass('disabled');
+        }
+        , reLoad: function () {
+            this.load();
+        }
+        , load: function (e, extend) {
+            console.info('Loading items');
+            var params = {};
+            params = (typeof extend === "object") ? $.extend({}, params, extend) : params;
+            this.render(params);
+        }
+        , render: function (params) {
+            var self = this;
+            var template = Template.template.load('resources/ingest', 'ingest');
+            var $container = $(Config.positions.main);
+            template.done(function (data) {
+                var handlebarsTemplate = Template.handlebars.compile(data);
+                var output = handlebarsTemplate({});
+                $container.html(output).promise().done(function () {
+                    self.afterRender();
+                });
+            });
+        }
+        , loadStorageFiles: function (e) {
+            typeof e !== "undefined" && e.preventDefault();
+            var template = Template.template.load('resources/ingest', 'storagefiles.partial');
+            var $container = $("#storagefiles-place");
+            var params = {path: '/files'};
+            var model = new IngestModel(params);
+            var self = this;
+            $container.fadeOut();
+            model.fetch({
+                data: $.param(params)
+                , success: function (items) {
+                    items = self.prepareItems(items.toJSON(), params);
+                    template.done(function (data) {
+                        var handlebarsTemplate = Template.handlebars.compile(data);
+                        var output = handlebarsTemplate(items);
+                        $container.html(output).promise().done(function () {
+                            $container.stop().fadeIn();
+                        });
+                    });
+                }
+                , error: function (e, data) {
+                    toastr.error(data.responseJSON.Message, 'خطا', {positionClass: 'toast-bottom-left', progressBar: true, closeButton: true});
+                }
+            });
+        }
+        , afterRender: function () {
+            this.loadStorageFiles();
+//            console.log(this.flags.treeLoaded)
+//            if (typeof this.flags.treeLoaded === "undefined") {
+            $("#tree").length && new Tree($("#tree"), Config.api.tree, this).render();
+//                this.flags.treeLoaded = true;
+//            }
+            $("#toolbar button[type=submit]").removeClass('hidden').addClass('in');
+            if (typeof this.flags.helperLoaded === "undefined") {
+                IngestHelper.init();
+                this.flags.helperLoaded = true;
+            } else
+                IngestHelper.init(true);
+            this.renderStatusbar();
+        }
+        , renderToolbar: function () {
+            var self = this;
+//            if (self.flags.toolbarRendered)
+//                return;
+            var elements = self.toolbar;
+            var toolbar = new Toolbar();
+            $.each(elements, function () {
+                var method = Object.getOwnPropertyNames(this);
+                toolbar[method](this[method]);
+            });
+            toolbar.render();
+//            self.flags.toolbarRendered = true;
+        }
+        , prepareItems: function (items, params) {
+            if (typeof items.query !== "undefined")
+                delete items.query;
+            if (typeof params !== "undefined") {
+                for (var prop in params) {
+                    delete items[prop];
+                }
+            }
+            return items;
+        }
+        , prepareContent: function () {
+            this.renderToolbar();
+        }
+        , prepareSave: function () {
+            var data = [{}];
+            $(this.$modal).find("input, textarea, select").each(function () {
+                var $input = $(this);
+                if (typeof $input.attr("name") !== "undefined") {
+                    data[0][$input.attr("name")] = (/^\d+$/.test($input.val()) || ($input.attr("data-validation") === 'digit')) ? +$input.val() : $input.val();
+                    if (typeof $input.attr('data-before-save') !== "undefined") {
+                        switch ($input.attr('data-before-save')) {
+                            case 'prepend-date':
+                                data[0][$input.attr("name")] = Global.jalaliToGregorian($(this).parent().find("label").text()) + 'T' + $input.val();
+                                break;
+                            case 'timestamp':
+                                data[0][$input.attr("name")] = Global.processTime($input.val());
+                                break;
+                        }
+                    }
+                }
+            });
+            return data;
+        }
+        , handleTreeCalls: function (routes, path) {
+            var self = this;
+            var pathId = routes.pop().toString();
+            var params = {overrideUrl: Config.api.metadata};
+            $("[data-type=path]").length && $("[data-type=path]").val(path.toString());
+            $("[data-type=path-id]").length && $("[data-type=path-id]").val(pathId.toString());
+
+            var template = Template.template.load('resources/ingest', 'metadata.partial');
+            var $container = $(self.$metadataPlace);
+            var model = new IngestModel(params);
+            model.fetch({
+                data: $.param({categoryId: pathId})
+                , success: function (data) {
+                    items = self.prepareItems(data.toJSON(), params);
+                    template.done(function (data) {
+                        var handlebarsTemplate = Template.handlebars.compile(data);
+                        var output = handlebarsTemplate(items);
+                        $container.html(output).promise().done(function () {
+//                            self.afterRender();
+                        });
+                    });
+                }
+//                , error: function (e, data) {
+//                    toastr.error(data.responseJSON.Message, 'خطا', {positionClass: 'toast-bottom-left', progressBar: true, closeButton: true});
+//                }
+            });
+        }
+        , renderStatusbar: function () {
+            var elements = this.statusbar;
+            var statusbar = new Statusbar();
+            $.each(elements, function () {
+                statusbar.addItem(this);
+            });
+            statusbar.render();
+        }
+    });
+    return IngestView;
+});
