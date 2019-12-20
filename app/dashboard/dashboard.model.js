85988f584e2546682ea1b4770f4b21c248db90a5
Farid Rn <faridv@gmail.com>
2019-12-20 16:21:04 +0330

    implemented missing overrideUrl for dashboard model

--- app/dashboard/dashboard.model.js
+++ app/dashboard/dashboard.model.js
@@ -6,10 +6,14 @@ define(["jquery", "underscore", "backbone", "config"
             this.query = (options && options.query) ? '?' + options.query : '';
             this.path = (options && options.path) ? options.path : '';
             this.path = (options && options.id) ? '/' + options.id : this.path;
+            this.overrideUrl = (options && options.overrideUrl) ? options.overrideUrl : '';
             options = {};
         }
         , url: function () {
-            return Config.api.dashboardSystem + this.path + this.query;
+            if (this.overrideUrl !== "")
+                return this.overrideUrl + this.path + this.query;
+            else
+                return Config.api.dashboardSystem + this.path + this.query;
         }
         , save: function (key, val, options) {
             return Backbone.Model.prototype.save.call(this, key, val, options);
