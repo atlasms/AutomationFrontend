45b2ffe9baebc3cffadf529dee710496573cd406
Farid Rn <faridv@gmail.com>
2020-10-03 02:10:26 +0330

    refactored tasks page

--- app/user/tasks/tasks.view.js
+++ app/user/tasks/tasks.view.js
@@ -2,34 +2,66 @@ define(['jquery', 'underscore', 'backbone', 'template', 'config', 'global', 'tas
 ], function ($, _, Backbone, Template, Config, Global, TasksModel, UsersManageModel, toastr, Toolbar, select2) {
     var TasksView = Backbone.View.extend({
         toolbar: [
+            // {'button': {cssClass: 'btn default pull-left', text: 'صندوق دریافتی‌ها', type: 'button', task: 'received', icon: 'fa fa-download'}},
+            // {'button': {cssClass: 'btn default pull-left', text: 'ارسالی‌ها', type: 'button', task: 'sent', icon: 'fa fa-upload'}},
+            {'button': {cssClass: 'btn btn-success', text: '', type: 'submit', task: 'filter', text: 'جستجو', icon: 'fa fa-search'}},
             {'button': {cssClass: 'btn purple-studio pull-right', text: '', type: 'button', task: 'refresh-view', icon: 'fa fa-refresh'}},
-            {'button': {cssClass: 'btn default pull-left', text: 'صندوق دریافتی‌ها', type: 'button', task: 'received', icon: 'fa fa-download'}},
-            {'button': {cssClass: 'btn default pull-left', text: 'ارسالی‌ها', type: 'button', task: 'sent', icon: 'fa fa-upload'}}
+            {
+                'select': {
+                    cssClass: 'form-control', name: 'mode', options: [
+                        {value: 'mytask', text: 'دریافتی‌ها', default: true},
+                        {value: 'sent', text: 'ارسالی‌ها'}
+                    ], addon: true, icon: 'fa fa-inbox', text: 'نوع'
+                }
+            },
+            {
+                'input': {
+                    cssClass: 'form-control datepicker', placeholder: 'تاریخ پخش', type: 'text', name: 'date', addon: true, icon: 'fa fa-calendar',
+                    value: Global.jalaliToGregorian(persianDate(SERVERDATE).format('YYYY-MM-DD')), style: 'max-width: 100px;', text: 'تاریخ پخش'
+                }
+            },
+            {
+                'select': {
+                    cssClass: 'form-control', name: 'filter-type', options: [
+                        {value: 'date', text: 'بر اساس تاریخ پخش'},
+                        {value: 'all', text: 'همه'}
+                    ], addon: true, icon: 'fa fa-list'
+                }
+            },
         ]
         , flags: {}
-        , cachedItems: []
+        , items: []
+        , filters: {
+            status: 'mytask',
+            date: Global.jalaliToGregorian(persianDate(SERVERDATE).format('YYYY-MM-DD')),
+            // type: 'date'
+        }
+        , defaultParams: {
+            masterid: 0,
+            touserid: 0,
+            togroupid: 0,
+            jobid: 0
+        }
+        , currentState: 'received'
         , currentAssigningItem: null
         , events: {
             'click .inbox-sidebar [data-type]': 'filterItems'
             , 'click .inbox-content table tr': 'loadTask'
             , 'click [data-task="refresh-view"]': 'reLoad'
-            , 'click [data-task="sent"]': 'redirectToSent'
-            , 'click [data-task="received"]': 'redirectToReceived'
-            , 'change [data-task="filter-status"]': 'filter'
-            , 'click [data-task="follow-up"]': 'followUp'
-
             , 'click [data-task="assign"]': 'openAssignModal'
             , 'click [data-task="assign-item"]': 'assign'
             , 'change [name="ToGroupId"]': 'updateUserList'
-
-            , 'click .enable-recommended-checkbox input[type="checkbox"]': 'enableRecommendedDate'
-            // , 'click [name="to-type"]': 'changeSendRecipient'
-
+            , 'click [data-task="follow-up"]': 'followUp'
             , 'click [data-task="load-media"]': 'loadMedia'
             , 'change [data-type="change-state"]': 'changeStatus'
             , 'click [data-type="change-state"]': function (e) {
                 e.stopPropagation();
             }
+
+            , 'change [data-type="mode"]': 'load'
+            , 'click [data-task="filter"]': 'filter'
+            , 'click [data-task="sent"]': 'filter'
+            , 'click [data-task="received"]': 'filter'
         }
         , changeStatus: function (e, givenParams, reload) {
             var $target = null;
@@ -141,63 +173,15 @@ define(['jquery', 'underscore', 'backbone', 'template', 'config', 'global', 'tas
             var win = window.open('/resources/mediaitem/' + mediaId, Config.mediaLinkTarget);
             win && win.focus();
         }
-        , filter: function (e, forcedState) {
-            var status = typeof forcedState !== 'undefined' && forcedState ? forcedState : ~~$(e.target).val();
-            var $rows = $('.inbox-body table:first tbody tr');
-            var filterByDate = $('.enable-recommended-checkbox input[type="checkbox"]')[0].checked;
-            var dateFilterValue = $('#filter-date').val();
-            if (status === 0) {
-                $rows.show(0);
-            } else {
-                $rows.hide(0);
-                $rows.each(function () {
-                    if (filterByDate) {
-                        if ($(this).is('[data-status="' + status + '"]') && $(this).is('[data-date="' + dateFilterValue + '"]')) {
-                            $(this).show();
-                        }
-                    } else {
-                        if ($(this).is('[data-status="' + status + '"]')) {
-                            $(this).show();
-                        }
-                    }
-                });
-            }
-        }
-        , enableRecommendedDate: function (e) {
-            var self = this;
-            var $input = $('#filter-date');
-            setTimeout(function () {
-                if (e.target.checked) {
-                    $input.prop('disabled', false);
-                } else {
-                    $input.prop('disabled', 'disabled');
-                }
-                self.filter(undefined, $('[data-task="filter-status"]').val());
-            }, 100);
-        }
-        , redirectToSent: function (e) {
-            e.preventDefault();
-            new Backbone.Router().navigate('/user/tasks/sent', {trigger: true});
-        }
-        , redirectToReceived: function (e) {
-            e.preventDefault();
-            new Backbone.Router().navigate('/user/tasks', {trigger: true});
-        }
-        , defaultParams: {
-            masterid: 0,
-            touserid: 0,
-            togroupid: 0,
-            jobid: 0
-        }
         , loadTask: function (e) {
             e.preventDefault();
             var self = this;
             var id = $(e.target).parents('tr:first').attr('data-id');
             var data = null;
-            Object.keys(this.cachedItems).forEach(function (key) {
-                console.log(self.cachedItems[key]);
-                if (self.cachedItems[key].Id === ~~id) {
-                    data = self.cachedItems[key];
+            Object.keys(this.items).forEach(function (key) {
+                console.log(self.items[key]);
+                if (self.items[key].Id === ~~id) {
+                    data = self.items[key];
                 }
             });
             if (data) {
@@ -211,35 +195,6 @@ define(['jquery', 'underscore', 'backbone', 'template', 'config', 'global', 'tas
                 });
             }
         }
-        , filterItems: function (e) {
-            e.preventDefault();
-            var $li = $(e.target).parents('li:first');
-            var type = $(e.target).is('a') ? $(e.target).attr('data-type') : $li.find('a:first').attr('data-type');
-            var $rows = $('.inbox-body table:first tbody tr');
-            $li.parents('ul:first').find('li.active').removeClass('active');
-            $li.addClass('active');
-            switch (type) {
-                case 'me':
-                    $rows.each(function () {
-                        if (!$(this).hasClass('to-user'))
-                            $(this).hide();
-                        else
-                            $(this).show();
-                    });
-                    break;
-                case 'groups':
-                    $rows.each(function () {
-                        if ($(this).hasClass('to-user'))
-                            $(this).hide();
-                        else
-                            $(this).show();
-                    });
-                    break;
-                default:
-                    $rows.show();
-                    break;
-            }
-        }
         , toggleReceiptType: function (e) {
 //            e.preventDefault();
             var self = this;
@@ -250,88 +205,110 @@ define(['jquery', 'underscore', 'backbone', 'template', 'config', 'global', 'tas
         , reLoad: function () {
             this.render();
         }
-        , getPageType: function () {
-            return (Backbone.history.getFragment().split("/").pop().split("?")[0].indexOf('sent') !== -1) ? 'sent' : 'received';
-        }
-        , render: function () {
-            var type = this.getPageType();
-            switch (type) {
-                case 'sent':
-                    this.loadSentItems();
-                    $('[data-task="sent"]').removeClass('default').addClass('blue');
-                    break;
-                case 'received':
-                    $('[data-task="received"]').removeClass('default').addClass('blue');
-                    this.loadReceivedItems();
-                    break;
+        , filter: function (e, callback) {
+            if (typeof e === 'object') {
+                e.preventDefault();
             }
-            return true;
+            var filters = this.getFilters();
+            console.log(filters);
+            var items = $.extend([], this.items);
+            for (var i = 0; i < items.length; i++) {
+                items[i].visible = false;
+                if (filters.date !== null) {
+                    if (items[i].date === filters.date) {
+                        items[i].visible = true;
+                    }
+                } else {
+                    items[i].visible = true;
+                }
+                if (filters.status !== 0) {
+                    if (items[i].Status !== filters.status && items[i].visible) {
+                        items[i].visible = false;
+                    }
+                }
+            }
+            if (typeof callback === 'function') {
+                callback();
+            } else {
+                this.showItems();
+            }
         }
-        , loadSentItems: function () {
+        , load: function () {
             var self = this;
-            var template = Template.template.load('user/tasks', 'tasks.sent');
-            var $container = $(Config.positions.main);
-            var params = {path: '/sent?status=0'};
+            var params = this.getLoadParams();
             new TasksModel(params).fetch({
                 success: function (data) {
-                    var items = self.prepareItems(data.toJSON(), {path: '/sent'});
-                    self.cachedItems = items;
-                    template.done(function (tmpl) {
-                        var handlebarsTemplate = Template.handlebars.compile(tmpl);
-                        var output = handlebarsTemplate(items);
-                        $container.html(output).promise().done(function () {
-                            self.afterRender();
-                        });
-                    });
+                    var items = self.prepareItems(data.toJSON(), params);
+                    self.items = items;
+                    self.filter();
                 }
             });
         }
-        , loadReceivedItems: function (callback) {
+        , showItems: function () {
+            var self = this;
+            var template = Template.template.load('user/tasks', 'task-items.partial');
+            var $container = $('#task-items');
+            template.done(function (tmpl) {
+                var handlebarsTemplate = Template.handlebars.compile(tmpl);
+                var output = handlebarsTemplate(self.items);
+                $container.html(output)
+            });
+        }
+        , getFilters: function () {
+            // return $.extend({}, this.filters, {
+            return {
+                status: ~~$('[name="status"]').val(),
+                date: $('[name="filter-type"]').val() === 'date' ? Global.jalaliToGregorian($('[name="date"]').val())[0] : null,
+                // type:
+            };
+        }
+        , getLoadParams: function () {
+            var mode = $('[data-type="mode"]').val();
+            return {
+                path: '/' + mode + '?status=0'
+            };
+        }
+        , render: function () {
             var self = this;
             var template = Template.template.load('user/tasks', 'tasks');
             var $container = $(Config.positions.main);
-            var params = {path: '/mytask?status=0'};
-            new TasksModel(params).fetch({
-                success: function (data) {
-                    var items = self.prepareItems(data.toJSON(), {path: '/mytask'});
-                    self.cachedItems = items;
-                    template.done(function (tmpl) {
-                        var handlebarsTemplate = Template.handlebars.compile(tmpl);
-                        var output = handlebarsTemplate(items);
-                        $container.html(output).promise().done(function () {
-                            self.afterRender();
-
-                            $('[data-task="filter-status"]').val('1');
-                            self.filter(undefined, 1);
-                        });
-                    });
-                }
+            template.done(function (tmpl) {
+                var handlebarsTemplate = Template.handlebars.compile(tmpl);
+                var output = handlebarsTemplate({});
+                $container.html(output).promise().done(function () {
+                    self.load();
+                });
             });
+            // this.load();
+            return true;
         }
         , afterRender: function () {
             this.loadUsersList();
             this.attachDatepickers();
         }
         , attachDatepickers: function () {
-            var self = this;
+            // var self = this;
             var $datePickers = $(".datepicker");
             $.each($datePickers, function () {
                 var $this = $(this);
                 if ($this.data('datepicker') == undefined) {
                     $this.pDatepicker($.extend({}, CONFIG.settings.datepicker, {
-                        onSelect: function () {
-                            self.filter(undefined, $('[data-task="filter-status"]').val());
-                        }
+                        // onSelect: function () {
+                        //     // self.filter(undefined, $('[data-task="status"]').val());
+                        // }
                     }));
                 }
             });
         }
         , renderToolbar: function () {
             var self = this;
-//            if (self.flags.toolbarRendered)
-//                return;
-            var elements = self.toolbar;
             var toolbar = new Toolbar();
+
+            var definedStates = toolbar.getDefinedToolbar(223, 'status', [{Value: '0', Key: 'همه'}]);
+            definedStates[0].select.text = 'وضعیت';
+            definedStates[0].select.options[1].default = true;
+
+            var elements = $.merge($.merge([], self.toolbar), definedStates);
             $.each(elements, function () {
                 var method = Object.getOwnPropertyNames(this);
                 toolbar[method](this[method]);
@@ -347,7 +324,12 @@ define(['jquery', 'underscore', 'backbone', 'template', 'config', 'global', 'tas
                     delete items[prop];
                 }
             }
-            return items;
+            var itemsList = Object.keys(items).map(function (k) {
+                items[k].visible = false;
+                items[k].date = Global.extractDate(items[k].RecommendedBroadcastDate);
+                return items[k];
+            });
+            return itemsList;
         }
         , prepareContent: function () {
             this.renderToolbar();
