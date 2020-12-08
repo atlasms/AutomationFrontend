50336b9f07537a4a722ee9eea3b3e56e23336524
Farid Rn <faridv@gmail.com>
2020-12-08 12:35:00 +0330

    updated hq media link url disposition

--- assets/js/helpers/template.js
+++ assets/js/helpers/template.js
@@ -381,13 +381,19 @@ define(['jquery', 'underscore', 'backbone', 'handlebars', 'config', 'global', 'm
                 });
                 return pathArray.join('\\');
             });
-            Handlebars.registerHelper('generateSafeFileName', function (param1, param2, param3, options) {
-                // var safeString = param0;
-                var safeString = '';
-                safeString += param1.replace(/[|&:;$%@"<>()+,]/g, '').replace(' ', '-');
-                safeString += '_' + Global.createTime2(param3).split(':').reverse().join('-');
-                safeString += '_کلاکت-' + param2;
+            Handlebars.registerHelper('generateSafeFileName', function (id, title, episodeNumber, duration, options) {
+                var template = Config.hqFilenameTemplate;
+                var safeString = template.replace(/{id}/, id)
+                    .replace(/{episodeNumber}/, episodeNumber)
+                    .replace(/{title}/, title.replace(/\s+/g, '-').replace(/[|&:;$%@"<>()+,]/g, ''))
+                    .replace(/{duration}/, Global.createTimeNoHours(duration));
                 return safeString;
+                // var safeString = param0;
+                // var safeString = '';
+                // safeString += param1.replace(/[|&:;$%@"<>()+,]/g, '').replace(' ', '-');
+                // safeString += '_' + Global.createTime2(param3).split(':').reverse().join('-');
+                // safeString += '_کلاکت-' + param2;
+                // return safeString;
             });
             Handlebars.registerHelper('$_get', function (property, date, options) {
                 var date = typeof date !== "undefined" ? date : false;
