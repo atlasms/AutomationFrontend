b82dc858662feac57bc21d7df15457013ee55ce6
Farid Rn <faridv@gmail.com>
2021-04-27 01:40:32 +0430

    improved tree

--- assets/js/helpers/tree.js
+++ assets/js/helpers/tree.js
@@ -4,62 +4,63 @@ define(['jquery', 'underscore', 'backbone', 'config', 'jstree', 'bootstrap/modal
     var Tree = function ($el, api, callback, options) {
         var $this = this;
         this.selected = {};
-        this.$el = (typeof $el !== "undefined") ? $el : $("tree");
+        this.$el = (typeof $el !== 'undefined') ? $el : $('tree');
         this.api = api.indexOf('//') !== -1 ? api : Config.api.url + api;
-        this.callback = (typeof callback !== "undefined") ? callback : null;
+        this.callback = (typeof callback !== 'undefined') ? callback : null;
         this.defaults = {
-            "core": {
-                "themes": {
-                    "responsive": false
+            core: {
+                themes: {
+                    responsive: false
                 }
                 // so that create works
-                , "check_callback": true
-                , 'data': {
-                    'url': function (node) {
+                , check_callback: true
+                , data: {
+                    url: function (node) {
                         return $this.api;
                     }
-                    , 'data': function (node) {
-                        return {'pid': node.id.replace('#', 0)};
+                    , data: function (node) {
+                        return { pid: node.id.replace('#', 0) };
                     }
                 }
             }
-            , "types": {
-                "default": {"icon": "fa fa-folder folder icon-state-warning icon-lg"}
-                , "file": {"icon": "fa fa-file file icon-state-warning icon-lg"}
+            , types: {
+                default: { icon: 'fa fa-folder folder icon-state-warning icon-lg' }
+                , file: { icon: 'fa fa-file file icon-state-warning icon-lg' }
             }
-            , "state": {"key": "tree"}
-            , "plugins": ["contextmenu", "state", "types", "search"]
-            , "checkbox": {
-                "tie_selection": false
+            , state: { 'key': 'tree' }
+            // , plugins: ['contextmenu', 'state', 'types', 'search', 'wholerow']
+            , plugins: ['contextmenu', 'state', 'types', 'search']
+            , checkbox: {
+                'tie_selection': false
             }
             , contextmenu: {
                 items: function (node) {
                     var Callees = {
                         destroy: function (node) {
-                            var params = {method: 'delete', id: node.toString()};
+                            var params = { method: 'delete', id: node.toString() };
                             bootbox.confirm({
-                                message: "آیا مطمئن هستید مورد انتخاب شده پاک شود؟"
+                                message: 'آیا مطمئن هستید مورد انتخاب شده پاک شود؟'
                                 , buttons: {
-                                    confirm: {className: 'btn-success'}
-                                    , cancel: {className: 'btn-danger'}
+                                    confirm: { className: 'btn-success' }
+                                    , cancel: { className: 'btn-danger' }
                                 }
                                 , callback: function (results) {
-                                    if ($this.callback && typeof $this.callback['handleTreeCallbacks'] !== "undefined")
+                                    if ($this.callback && typeof $this.callback['handleTreeCallbacks'] !== 'undefined')
                                         if (results)
                                             $this.callback['handleTreeCallbacks'](params, $($this.$el));
                                 }
                             });
                         },
                         undelete: function (node) {
-                            var params = {method: 'undelete', id: node.id.toString()};
+                            var params = { method: 'undelete', id: node.id.toString() };
                             bootbox.confirm({
-                                message: "آیا مطمئن هستید مورد انتخاب شده برگردانده شود؟"
+                                message: 'آیا مطمئن هستید مورد انتخاب شده برگردانده شود؟'
                                 , buttons: {
-                                    confirm: {className: 'btn-success'}
-                                    , cancel: {className: 'btn-danger'}
+                                    confirm: { className: 'btn-success' }
+                                    , cancel: { className: 'btn-danger' }
                                 }
                                 , callback: function (results) {
-                                    if ($this.callback && typeof $this.callback['handleTreeCallbacks'] !== "undefined")
+                                    if ($this.callback && typeof $this.callback['handleTreeCallbacks'] !== 'undefined')
                                         if (results)
                                             $this.callback['handleTreeCallbacks'](params, $($this.$el));
                                 }
@@ -69,15 +70,15 @@ define(['jquery', 'underscore', 'backbone', 'config', 'jstree', 'bootstrap/modal
                     var contextItems = {};
                     if (Authorize.access(8)) {
                         contextItems.Create = {
-                            "label": "مورد جدید"
+                            label: 'مورد جدید'
                             , icon: 'fa fa-plus'
-                            , "action": function (data) {
+                            , action: function (data) {
                                 var ref = $.jstree.reference(data.reference);
                                 sel = ref.get_selected();
                                 if (!sel.length || ref.is_closed(sel))
                                     return false;
                                 sel = sel[0];
-                                sel = ref.create_node(sel, {"type": "folder"});
+                                sel = ref.create_node(sel, { 'type': 'folder' });
                                 if (sel)
                                     ref.edit(sel);
                             }
@@ -85,9 +86,9 @@ define(['jquery', 'underscore', 'backbone', 'config', 'jstree', 'bootstrap/modal
                     }
                     if (Authorize.access(16)) {
                         contextItems.Rename = {
-                            "label": "تغییر نام"
+                            'label': 'تغییر نام'
                             , icon: 'fa fa-pencil'
-                            , "action": function (data) {
+                            , 'action': function (data) {
                                 var inst = $.jstree.reference(data.reference);
                                 obj = inst.get_node(data.reference);
                                 inst.edit(obj);
@@ -96,7 +97,7 @@ define(['jquery', 'underscore', 'backbone', 'config', 'jstree', 'bootstrap/modal
                     }
                     if (Authorize.access(32)) {
                         contextItems.Delete = {
-                            label: "حذف"
+                            label: 'حذف'
                             , icon: 'fa fa-trash'
                             , action: function (data) {
                                 var ref = $.jstree.reference(data.reference),
@@ -114,8 +115,8 @@ define(['jquery', 'underscore', 'backbone', 'config', 'jstree', 'bootstrap/modal
                         delete contextItems.Create;
                         delete contextItems.Delete;
                         contextItems.Acticate = {
-                            label: "un-delete"
-                            , icon: "fa fa-check"
+                            label: 'un-delete'
+                            , icon: 'fa fa-check'
                             , action: function (data) {
                                 // alert('UNDELETE!')
                                 var ref = $.jstree.reference(data.reference),
@@ -136,73 +137,80 @@ define(['jquery', 'underscore', 'backbone', 'config', 'jstree', 'bootstrap/modal
                 }
             }
             , search: {
-
+                case_insensitive: true,
+                show_only_matches: true
             }
         };
 
         if (Authorize.access(64))
-            this.defaults.plugins.push("dnd");
-        if (options && typeof options.hasCheckboxes !== "undefined" && options.hasCheckboxes)
-            this.defaults.plugins.push("checkbox");
+            this.defaults.plugins.push('dnd');
+        if (options && typeof options.hasCheckboxes !== 'undefined' && options.hasCheckboxes)
+            this.defaults.plugins.push('checkbox');
 
         this.options = $.extend({}
             , this.defaults, options);
     };
     _.extend(Tree.prototype, {
         render: function () {
-            var $this = this;
-            $($this.$el).jstree($this.options);
+            var self = this;
+            var $tree = $(self.$el);
+            $tree.jstree(self.options);
             // Tree Event Listeners
-            $($this.$el).on('select_node.jstree', function (e, data) {
+            $tree.on('select_node.jstree', function (e, data) {
                 var i, j, r = [];
                 for (i = 0, j = data.selected.length; i < j; i++)
                     r.push(data.instance.get_node(data.selected[i]).id);
 
-                $this.selected.id = data.instance.get_node(data.selected[0]).id;
-                $this.selected.text = data.instance.get_node(data.selected[0]).text;
+                self.selected.id = data.instance.get_node(data.selected[0]).id;
+                self.selected.text = data.instance.get_node(data.selected[0]).text;
 
                 var file_data = [];
                 var selectedNodes = data.instance.get_selected();
                 for (var i = 0; i < selectedNodes.length; i++) {
                     var full_node = data.instance.get_node(selectedNodes[i]);
-                    file_data[i] = data.instance.get_path(full_node, "/");
+                    file_data[i] = data.instance.get_path(full_node, '/');
                 }
                 // Sending proccesses to callback methos
-                if ($this.callback && typeof $this.callback['handleTreeCalls'] !== "undefined")
-                    $this.callback['handleTreeCalls'](r, file_data);
+                if (self.callback && typeof self.callback['handleTreeCalls'] !== 'undefined')
+                    self.callback['handleTreeCalls'](r, file_data);
             });
-            $($this.$el).on('after.jstree', function (e, data) {
+            $tree.on('after.jstree', function (e, data) {
             });
-            $($this.$el).on('loaded.jstree', function (e, data) {
+            $tree.on('loaded.jstree', function (e, data) {
 //                console.log(e, data);
             });
 //            $($this.$el).on('before.jstree', function (e, data) {
 //                
 //            });
-            $($this.$el).on('ready.jstree', function (e, data) {
+            $tree.on('ready.jstree', function (e, data) {
                 window.setTimeout(function () {
-                    var params = {method: 'ready', id: $this.selected.id, task: '', text: $this.selected.text, parent: null};
-                    if ($this.callback && typeof $this.callback['handleTreeCallbacks'] !== "undefined")
-                        $this.callback['handleTreeCallbacks'](params, $($this.$el));
+                    var params = { method: 'ready', id: self.selected.id, task: '', text: self.selected.text, parent: null };
+                    if (self.callback && typeof self.callback['handleTreeCallbacks'] !== 'undefined')
+                        self.callback['handleTreeCallbacks'](params, $(self.$el));
                 }, 500);
             });
-            $($this.$el).on('create_node.jstree', function (node, parent, position) {
+            $tree.on('create_node.jstree', function (node, parent, position) {
                 // Do nothing! 
             });
-            $($this.$el).on('rename_node.jstree', function (e, data) {
-                var params = {method: 'put', id: null, task: 'rename', text: data.text, parent: data.node.parent.replace("#", 0)};
+            $tree.on('rename_node.jstree', function (e, data) {
+                var params = { method: 'put', id: null, task: 'rename', text: data.text, parent: data.node.parent.replace('#', 0) };
                 if (data.node.id.indexOf('j') !== -1 && data.node.id.indexOf('_') !== -1)
                     params.method = 'post';
                 else
                     params.id = data.node.id;
-                if ($this.callback && typeof $this.callback['handleTreeCallbacks'] !== "undefined")
-                    $this.callback['handleTreeCallbacks'](params, $($this.$el), data.node);
+                if (self.callback && typeof self.callback['handleTreeCallbacks'] !== 'undefined')
+                    self.callback['handleTreeCallbacks'](params, $(self.$el), data.node);
             });
-            $($this.$el).on('move_node.jstree', function (e, data) {
-                var params = {method: 'put', id: data.node.id, task: 'dnd', text: data.node.text, parent: data.parent.replace("#", 0)};
-                if ($this.callback && typeof $this.callback['handleTreeCallbacks'] !== "undefined")
-                    $this.callback['handleTreeCallbacks'](params, $($this.$el), data.node);
+            $tree.on('move_node.jstree', function (e, data) {
+                var params = { method: 'put', id: data.node.id, task: 'dnd', text: data.node.text, parent: data.parent.replace('#', 0) };
+                if (self.callback && typeof self.callback['handleTreeCallbacks'] !== 'undefined')
+                    self.callback['handleTreeCallbacks'](params, $(self.$el), data.node);
             });
+            $tree.on('search.jstree', function (nodes, str, res) {
+                if (str.nodes.length === 0) {
+                    $tree.jstree(true).hide_all();
+                }
+            })
         }
     });
 
